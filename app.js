b8452fccae864ef3cb6d3c72e08cd1782c8f1f37
Krittika-Saha <krittika.saha.dev@gmail.com>
2020-11-09 14:05:38 +0530

    Carousel and navbar added

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', function(req, res) {
+  res.render('home')
+});
+
+app.listen(9000, function() {
+  console.log("Server started on port 9000.");
+});
